b067250d7e78445ee0ea191400f6ce1b7a5b137f
harishmanikbtg <harishbhurebtg@gmail.com>
2023-05-12 12:48:08 +0530

    moved state to app. displayed status message. calculate winner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import './style.scss'
-import Board from './components/Board'
+import './style.scss';
+import Board from './components/Board';
+import { useState } from 'react';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setXNext] = useState(false);
+  const winner = calculateWinner(squares);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+
+    setXNext(currentIsNext => !currentIsNext);
+  };
 
   return (
-    <div className='app'>
-      <Board/>
+    <div className="app">
+      <h2>{statusMessage} </h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
